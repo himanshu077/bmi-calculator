e08448ca858196cee0de0e8ca18ca3e6c9331542
himanshu077 <himanshukumar077@gmail.com>
2024-02-16 18:45:59 +0530

    added kids bmi according to percentile

--- lib/services/repository/mesaurementRepo/MeasurementRepoImplementation.dart
+++ lib/services/repository/mesaurementRepo/MeasurementRepoImplementation.dart
@@ -6,42 +6,45 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import '../../../model/KidsBmiModel.dart';
 import 'MeasurementRepo.dart';
 
-class MeasurementRepoImplementation  implements MeasurementRepo{
+class MeasurementRepoImplementation implements MeasurementRepo {
   final _collection = FirebaseDBService('data', 'measurement');
 
   @override
-  Future<String> calculate({
-    required MeasurementModel doc
-}) async{
-    try{
-      return await _collection.setDoc(doc.id!,doc.toJson());
-    }catch(e){
+  Future<String> calculate({required MeasurementModel doc}) async {
+    try {
+      return await _collection.setDoc(doc.id!, doc.toJson());
+    } catch (e) {
       rethrow;
     }
   }
 
   @override
-  Future<void> deleteHistoryItem({required String id}) async{
+  Future<void> deleteHistoryItem({required String id}) async {
     return await _collection.delete(id);
   }
 
   @override
-  Future<List<MeasurementModel>> fetchHistory() async{
-    try{
-     final snapShot = await _collection.documentsWithOrderWhere(
-       compareField: 'uid',
-         compareValue: AppData.uid,
-         sortField: 'timeStamp',descending: true);
-     if(snapShot.size > 0){
-       List<MeasurementModel> ll = List<MeasurementModel>.from(snapShot.docs.map((doc) => MeasurementModel.fromJson(doc.data() as Map<String,dynamic>)));
-       ll.sort((a, b) => b.timeStamp!.compareTo(a.timeStamp!),);
-       return ll;
-     }else{
-       return [];
-     }
-    }on FirebaseException catch(e){
+  Future<List<MeasurementModel>> fetchHistory() async {
+    try {
+      final snapShot = await _collection.documentsWithOrderWhere(
+          compareField: 'uid',
+          compareValue: AppData.uid,
+          sortField: 'timeStamp',
+          descending: true);
+      if (snapShot.size > 0) {
+        List<MeasurementModel> ll = List<MeasurementModel>.from(snapShot.docs
+            .map((doc) =>
+                MeasurementModel.fromJson(doc.data() as Map<String, dynamic>)));
+        ll.sort(
+          (a, b) => b.timeStamp!.compareTo(a.timeStamp!),
+        );
+        return ll;
+      } else {
+        return [];
+      }
+    } on FirebaseException catch (e) {
       rethrow;
-    } catch(e){
+    } catch (e) {
       rethrow;
     }
   }
@@ -51,13 +54,9 @@ class MeasurementRepoImplementation  implements MeasurementRepo{
     // TODO: implement showResult
     throw UnimplementedError();
   }
-
 }
 
-
-
-MeasurementModel generateBMI(MeasurementModel data){
-
+MeasurementModel generateBMI(MeasurementModel data) {
   final gender = data.genderValue;
   final height = data.heightInMeter;
   final weight = data.weightInKg;
@@ -66,39 +65,37 @@ MeasurementModel generateBMI(MeasurementModel data){
   final bmi = weight / (height * height);
   data.bmiValue = bmi;
   BmiRemarksEnum remarks = BmiRemarksEnum.Underweight;
-  if(age > 19){
-    if(gender == GenderEnum.male){
-      if(bmi < 18.5){
+  if (age > 19) {
+    if (gender == GenderEnum.male) {
+      if (bmi < 18.5) {
         remarks = BmiRemarksEnum.Underweight;
-      }else if(bmi >= 18.5 && bmi < 25.0){
+      } else if (bmi >= 18.5 && bmi < 25.0) {
         remarks = BmiRemarksEnum.Normal;
-      }
-      else if(bmi >= 25.0 && bmi < 30.0){
+      } else if (bmi >= 25.0 && bmi < 30.0) {
         remarks = BmiRemarksEnum.Overweight;
-      }else{
+      } else {
         remarks = BmiRemarksEnum.Obese;
       }
-    }else{
-      if(bmi < 18.5){
+    } else {
+      if (bmi < 18.5) {
         remarks = BmiRemarksEnum.Underweight;
-      }else if(bmi >= 18.5 && bmi < 25.0){
+      } else if (bmi >= 18.5 && bmi < 25.0) {
         remarks = BmiRemarksEnum.Normal;
-      }
-      else if(bmi >= 25.0 && bmi < 30.0){
+      } else if (bmi >= 25.0 && bmi < 30.0) {
         remarks = BmiRemarksEnum.Overweight;
-      }else{
+      } else {
         remarks = BmiRemarksEnum.Obese;
       }
     }
-  }else{
+  } else {
     // get percentile for kids according to their bmi....
     if (gender == GenderEnum.male) {
       final percentileData =
-      boyPercentileList.firstWhere((element) => element.age == age);
+          boyPercentileList.firstWhere((element) => element.age == age);
       remarks = getRemarksByPercentile(percentileData, bmi);
     } else {
       final percentileData =
-      girlPercentileList.firstWhere((element) => element.age == age);
+          girlPercentileList.firstWhere((element) => element.age == age);
       remarks = getRemarksByPercentile(percentileData, bmi);
     }
   }
@@ -107,7 +104,6 @@ MeasurementModel generateBMI(MeasurementModel data){
 
   return data;
 }
-
 
 BmiRemarksEnum getRemarksByPercentile(KidsBmiModel data, double bmi) {
   BmiRemarksEnum remarks = BmiRemarksEnum.Underweight;
